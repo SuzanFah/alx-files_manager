966c9fef52900f0f0ad57340e0c304bb2f5a6303
SuzanFah <susiefahim@gmail.com>
2024-12-11 18:02:43 -0800

    Initial commit

---
+++ utils/redis.js
@@ -0,0 +1,34 @@
+mport redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+	  constructor() {
+		      this.client = redis.createClient();
+		      this.client.on('error', (error) => {
+			            console.log(`Redis client error: ${error}`);
+			          });
+		    }
+
+	  isAlive() {
+		      return this.client.connected;
+		    }
+
+	  async get(key) {
+		      const asyncGet = promisify(this.client.get).bind(this.client);
+		      const value = await asyncGet(key);
+		      return value;
+		    }
+
+	  async set(key, value, duration) {
+		      const asyncSet = promisify(this.client.setex).bind(this.client);
+		      await asyncSet(key, duration, value);
+		    }
+
+	  async del(key) {
+		      const asyncDel = promisify(this.client.del).bind(this.client);
+		      await asyncDel(key);
+		    }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
